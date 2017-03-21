a95459dc6122e4623700008661609f642f0d6b82
AcostaDemian <acostademianariel@gmail.com>
2017-03-21 17:47:16 -0300

    get/post/delete funciona, falta el put

--- index.php
+++ index.php
@@ -3,19 +3,80 @@
     use \Psr\Http\Message\ResponseInterface as Response;
 
     require 'vendor/autoload.php';
+    require 'Clases/Personas.php';
 
     $app = new \Slim\App;
-    
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write("Hola, vos quien sos?");
 
-        return $response;
+    //http://localhost:8080/PPS/index.php/personas    
+    $app->get('/personas', function ($req, $res, $args) {
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                Persona::TraerTodasLasPersonas()
+            )
+        );        
     });
 
-    $app->get('/hola/{name}', function (Request $request, Response $response) {
-        $name = $request->getAttribute('name');
-        $response->getBody()->write("Hola, $name");
+    //http://localhost:8080/PPS/index.php/persona/1
+    $app->get('/persona/{id}', function ($req, $res, $args) {
 
-        return $response;
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                Persona::TraerUnaPersona($args['id'])
+            )
+        );
     });
+
+    //http://localhost:8080/PPS/index.php/persona/borrar/3
+    $app->delete('/persona/borrar/{id}', function ($req, $res, $args) {        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                Persona::BorrarPersona($args['id'])
+            )
+        );
+    });
+
+    //http://localhost:8080/PPS/index.php/persona/guardar?nombre=Juan&apellido=Pedro&dni=1515
+    $app->post('/persona/guardar', function ($req, $res) {
+        $perConst = new Persona();
+        $perConst->SetNombre($req->getParam('nombre'));
+        $perConst->SetApellido($req->getParam('apellido'));
+        $perConst->SetDni($req->getParam('dni'));
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                Persona::InsertarPersona($perConst)
+            )
+        );
+    });
+
+    //http://localhost:8080/PPS/index.php/persona/modificar?id=5&nombre=Julio&apellido=Pedro
+    $app->put('/persona/modificar', function ($req, $res) {
+        $perConst = new Persona();
+        $perConst->SetId($req->getParam('id'));
+        $perConst->SetNombre($req->getParam('nombre'));
+        $perConst->SetApellido($req->getParam('apellido'));
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                Persona::ModificarPersona($perConst)
+            )
+        );
+    });
+
     $app->run();
